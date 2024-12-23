20b99fbc11c7232d081bf4ea8e3765bfe703fe15
Dhruva Yellampalli Kidambi <z5587728@ad.unsw.edu.au>
2024-12-23 11:55:51 +1100

    Added generalisation to add an array of tickers to analyse

--- mean-reversion/main.py
+++ mean-reversion/main.py
@@ -15,8 +15,14 @@ tickers = [
   "TSLA"
 ]
 
+ticker_data = {}
 
 for ticker in tickers:
   close_price[ticker] = yf.download(tickers = ticker, period = '6mo', interval= '1mo')['Adj Close']
   
-close_price.to_csv('ticker_data.csv')
+for ticker in tickers:
+  ticker_data[ticker] = yf.download(tickers = ticker, period = '6mo', interval= '1mo')
+  
+
+close_price.to_csv('close_price_data.csv')
+ticker_data.to_csv('ticker_data.csv')
