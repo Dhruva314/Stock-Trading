4ec04423bec418caba8448cd9d609fde78a27607
Dhruva Yellampalli Kidambi <z5587728@ad.unsw.edu.au>
2024-12-23 11:50:33 +1100

    Added R code to view data structure in RStudios

--- mean-reversion/data_viewer.R
+++ mean-reversion/data_viewer.R
@@ -5,3 +5,9 @@ while (TRUE) {
   View(ticker_data)
   Sys.sleep(60)   # Wait for 1 minute before refreshing
 }
+
+
+# Code for Console
+# library(readr)
+# View(read_csv("//wsl.localhost/Ubuntu/home/dhruva/ticker_data.csv"))
+
