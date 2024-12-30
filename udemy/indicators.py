5b96ed52b51e4011e5dd8981d3937731da9c4112
Dhruva Yellampalli Kidambi <z5587728@ad.unsw.edu.au>
2024-12-30 16:49:59 +1100

    Added the stubs for functions in a file that calculates technical indicator

---
+++ udemy/indicators.py
@@ -0,0 +1,26 @@
+# File with functions to calculate the value of common Technical Indicators (TI)
+
+def MACD(DF, f_period=12, s_period=26, ewm_span=9):
+  df = DF.copy()
+  return df
+
+def ATR(DF, ewm_span=14):
+  df = DF.copy()
+  return df
+
+def BollBands(DF, ewm_span=14):
+  df = DF.copy()
+  return df
+
+def RSI(DF, ewm_span=14):
+  df = DF.copy()
+  return df
+
+def ADX(DF, ewm_span=20):
+  df = DF.copy()
+  return df
+
+def Renko(DF, hourly_df):
+  df = DF.copy()
+  return df
+
