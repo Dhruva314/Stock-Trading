63a444e9ac4770d16adc8b09845d78aae1a6834a
Dhruva Yellampalli Kidambi <z5587728@ad.unsw.edu.au>
2025-01-02 14:26:05 +1100

    Created file for KPIs including stubs and description

---
+++ udemy/KPIs.py
@@ -0,0 +1,35 @@
+def CAGR(DF):
+  "function to calculate the Cumulative Annual Growth Rate of a trading strategy"
+  df = DF.copy()
+  
+  return df
+
+def volatility(DF):
+  "function to calculate annualized volatility of a trading strategy"
+  df = DF.copy()
+
+  return df
+
+def sharpe(DF, rf):
+  "function to calculate Sharpe Ratio of a trading strategy"
+  df = DF.copy()
+
+  return df
+
+def sortino(DF, rf):
+  "function to calculate Sortino Ratio of a trading strategy"
+  df = DF.copy()
+
+  return df
+
+def max_dd(DF):
+  "function to calculate max drawdown"
+  df = DF.copy()
+
+  return df
+
+def calmar(DF):
+  "function to calculate calmar ratio"
+  df = DF.copy()
+  
+  return df
