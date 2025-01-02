5f34be39946664234616c627433c703bcf7c3059
Dhruva Yellampalli Kidambi <z5587728@ad.unsw.edu.au>
2025-01-02 14:51:42 +1100

    implemented CAGR function

--- udemy/KPIs.py
+++ udemy/KPIs.py
@@ -1,8 +1,11 @@
 def CAGR(DF):
   "function to calculate the Cumulative Annual Growth Rate of a trading strategy"
   df = DF.copy()
-  
-  return df
+  df["return"] = DF["Adj Close"].pct_change()
+  df["cum_return"] = (1 + df["return"]).cumprod()
+  n = len(df)/252
+  CAGR = (df["cum_return"][-1])**(1/n) - 1
+  return CAGR
 
 def volatility(DF):
   "function to calculate annualized volatility of a trading strategy"
