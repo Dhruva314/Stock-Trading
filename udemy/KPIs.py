9db03b5328f7c952eb53366848aae6978445d2f3
Dhruva Yellampalli Kidambi <z5587728@ad.unsw.edu.au>
2025-01-02 14:53:20 +1100

    Implemented volitility function

--- udemy/KPIs.py
+++ udemy/KPIs.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def CAGR(DF):
   "function to calculate the Cumulative Annual Growth Rate of a trading strategy"
   df = DF.copy()
@@ -9,9 +11,11 @@ def CAGR(DF):
 
 def volatility(DF):
   "function to calculate annualized volatility of a trading strategy"
+  "DF must be in daily closes"
   df = DF.copy()
-
-  return df
+  df["return"] = df["Close"].pct_change()
+  vol = df["return"].std() * np.sqrt(252)
+  return vol
 
 def sharpe(DF, rf):
   "function to calculate Sharpe Ratio of a trading strategy"
